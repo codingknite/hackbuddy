e40df7b6fb6b589bd3f71241df5edebc047c1122
mojpm <itsjoelpm@gmail.com>
2021-09-27 13:52:30 +0300

    add sub replies test data

---
+++ api/utils/testHelpers/subReplies.js
@@ -0,0 +1,23 @@
+const testSubReplies = [
+  {
+    author: 'Jonathan Hex',
+    profilePic: 'https://exmaple.com/pic.png',
+    body: 'Great Job',
+  },
+  {
+    author: 'Mark Robertson',
+    profilePic: 'https://exmaple.com/pic.png',
+    body: 'Thanks for the update',
+  },
+];
+
+const testSubReply = {
+  author: 'Mark Robertson',
+  profilePic: 'https://exmaple.com/pic.png',
+  body: 'Thanks for the update',
+};
+
+module.exports = {
+  testSubReplies,
+  testSubReply,
+};
